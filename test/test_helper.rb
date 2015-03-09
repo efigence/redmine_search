ef406d1063fa89e641d7c87166371e5a2e6959ab
swiatkiewicz <m.swiatkiewicz91@gmail.com>
2015-03-09 15:39:26 +0100

    travis fix

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,24 @@
 # Load the Redmine helper
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
+
 def set_plugin_fixtures_attachments_directory
     Attachment.storage_path = "#{Rails.root}/plugins/redmine_search/test/fixtures/files"
+end
+
+class ActionController::TestCase
+
+  # XXX: redmine master branch is not compatible with other branches
+  fx = [:attachments, :projects, :issues, :members]
+  fx2 = [:users]
+
+  fixtures *fx
+  fixtures *fx2
+
+  if Gem::Version.new(Rails.version) >= Gem::Version.new('4.0')
+    ActiveRecord::Fixtures.create_fixtures(File.dirname(__FILE__) + '/fixtures/master/', fx2)
+  else
+    ActiveRecord::Fixtures.create_fixtures(File.dirname(__FILE__) + '/fixtures/default/', fx2)
+  end
+
+  ActiveRecord::Fixtures.create_fixtures(File.dirname(__FILE__) + '/fixtures/', fx)
 end
